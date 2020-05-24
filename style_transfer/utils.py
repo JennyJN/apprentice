41a2bb6c0cf0657da214f58bd18feaec4549fcb9
David F. Li <davfli@github.com>
2020-05-24 12:23:31 -0400

    adding data procssing utilit functions

--- style_transfer/utils.py
+++ style_transfer/utils.py
@@ -1,7 +1,39 @@
-class MyStyle(object):
+import cv2
+import numpy as np
+from matplotlib.pyplot import imshow
+import os
 
-	def __init__(self):
-		self.name = 'style transfer'
+class Image(object):
+	"""wrapper of loading/showing image"""
+	def __init__(self, path='', pixel=None):
+		if bool(path):
+			self.img = cv2.imread(path)
+		elif bool(pixel):
+			self.img = pixel
 
-	def readimg(self, path=''):
-		return path
+		self.size = self.img.shape
+
+	def __call__(self, *args):
+		self.show(*args)
+
+	def show(self, *args):
+		if bool(args):
+			img = cv2.resize(self.img, args)
+		else:
+			img = self.img
+
+		imshow(img)
+
+
+def load_folder(folder):
+	""" load all images from folder """
+	out = []
+	files = os.listdir(folder)
+	for filename in files:
+		if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
+			fp = os.path.join(folder, filename)
+			img = Image(path=fp)
+			out.append(img)
+
+	return out
+
